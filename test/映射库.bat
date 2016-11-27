9288540a07c109236d3814ac371bfec86117e5db
ziniulian <ziniulian@163.com>
2016-11-28 01:39:05 +0800

    平板更新

--- test/映射库.bat
+++ test/映射库.bat
@@ -1,5 +1,4 @@
 @echo off
-mklink /D "F:\myLib" "F:\GitLib\LZR_JS\src\LZR\JS"
-mklink /D "F:\GitLib\LjsApp\myLib" "F:\GitLib\LZR_JS\src\LZR\JS"
-mklink /D "F:\GitLib\LZR_JS\test\myLib" "F:\GitLib\LZR_JS\src\LZR\JS"
+mklink /D "L:\myLib" "L:\Doc\Git\LZR_JS\src\LZR\JS"
+mklink /D "L:\Doc\Git\LZR_JS\test\myLib" "L:\Doc\Git\LZR_JS\src\LZR\JS"
 pause
