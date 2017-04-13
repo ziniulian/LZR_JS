007e2a23aa3dc19ec1e27d0cb7e2ec3b09d3f39f
ziniulian <ziniulian@163.com>
2017-04-13 10:40:51 +0800

    gx

--- test/映射库.bat
+++ test/映射库.bat
@@ -1,4 +1,4 @@
 @echo off
-mklink /D "L:\myLib" "L:\Doc\Git\LZR_JS\src\LZR\JS"
-mklink /D "L:\Doc\Git\LZR_JS\test\myLib" "L:\Doc\Git\LZR_JS\src\LZR\JS"
+mklink /D "D:\myLib" "D:\Doc\Git\LZR_JS\src\LZR\JS"
+mklink /D "D:\Doc\Git\LZR_JS\test\myLib" "D:\Doc\Git\LZR_JS\src\LZR\JS"
 pause
