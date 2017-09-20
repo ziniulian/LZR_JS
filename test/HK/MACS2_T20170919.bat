8c589ce8fbe0cf0110860d47b1ed143bc2d237c0
ziniulian <ziniulian@163.com>
2017-09-20 08:12:45 +0800

    批处理查询局域网内用户信息

---
+++ test/HK/MACS2_T20170919.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+:: 用 nbtstat 方式获取局域网内所有电脑的MAC地址及电脑名。
+::如果不用 ping 可以检测到防止 ping 的主机，但这样时间消耗很大；
+::先 ping 到主机后，再执行 nbtstat 可以节约一点时间。
+
+echo %date% %time% > MACS2.txt
+set ip=192.169.0
+set rdo=0
+
+for /l %%s in (4,1,254) do (
+	echo %%s
+	ping %ip%.%%s -n 1 -w 100 | find "TTL" > nul && (
+		set rdo=0
+		for /F "tokens=1,2,3,4*" %%i in ('nbtstat -a %ip%.%%s ^| findstr "<00> MAC" ^| findstr /v "组"') do (
+			::echo %%i,%%j,%%k,%%l
+			if not !rdo!==1 (
+				if {%%k}=={=} (
+					echo %ip%.%%s	%%l		!rdo! >> MACS2.txt
+					set rdo=1
+				) else (
+					set rdo=%%i
+				)
+			)
+		)
+	)
+)
+exit
